8fb1bf1d295f25f61a47d67daa1e63d9e94a33ca
unitycoder <support@unitycoder.com>
2014-05-21 22:23:32 +0300

    updated demoscene

---
+++ Assets/Scripts/DemoResetScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoResetScene : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown("r")) Application.LoadLevel(0);
+
+	}
+}
